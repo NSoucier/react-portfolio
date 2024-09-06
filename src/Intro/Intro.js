003c0f21ce8697d704a75adc5e00f540310fec10
Nathan Soucier <soucienp@gmail.com>
2024-09-06 10:34:38 -0400

    updated about me section

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -18,12 +18,12 @@ function Intro() {
             <p id='bio'>
                 <h1>About me</h1>
                 
-                Hi! I am an experienced software developer with a focus on full-stack development and a passion for delivering end-to-end solutions.
+                I am an experienced software developer with a focus on full-stack development and a passion for delivering end-to-end solutions.
                 Leveraging a solid engineering background with hands-on experience in electrical and biomedical engineering, I bring a unique 
                 perspective to software development, blending technical proficiency with practical insights.
                 <br/> <br/>
-                Over several years in the industry, coupled with dedicated coursework and diverse projects, I've honed my skills across a wide range
-                of technologies, including Javascript, Python, CSS, HTML, React, Node.js, Express.js, SQL, JQuery, C++, Assembly, and Java. With 
+                Over several years in the engineering industry, coupled with dedicated coursework and diverse projects, I've honed my skills across a wide range
+                of technologies, including Javascript, Python, CSS, HTML, React, SQL, C++, Assembly, and Java. With 
                 expertise in both front-end and back-end technologies, I thrive in dynamic environments where innovation and collaboration are 
                 paramount.
                 <br/> <br/>
