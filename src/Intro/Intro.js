43516ca0c75903fe6ea5984c6eb70264d6b88b73
Nathan Soucier <soucienp@gmail.com>
2024-09-06 10:29:53 -0400

    updated bio and welcome message

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -4,21 +4,31 @@ import headshot from '../Images/headshot.png';
 function Intro() {
     return (
         <div id="intro">
-            <img src={headshot} id='headshot' />
+            <div>
+                <img src={headshot} id='headshot' />
+                <div id='welcome'>
+                    HELLO! 
+                    <br/>
+                    <span style={{ fontWeight: 600, fontSize: 'x-large' }}>I'm Nathan</span>
+                    <br/>
+                    Software Engineer
+                </div>                
+            </div>
+
             <p id='bio'>
-                <b>About.</b>
-                <br/> <br/>
-                &emsp; Hi! I am an experienced software developer with a focus on full-stack development and a passion for delivering end-to-end solutions.
+                <h1>About me</h1>
+                
+                Hi! I am an experienced software developer with a focus on full-stack development and a passion for delivering end-to-end solutions.
                 Leveraging a solid engineering background with hands-on experience in electrical and biomedical engineering, I bring a unique 
                 perspective to software development, blending technical proficiency with practical insights.
                 <br/> <br/>
-                &emsp; Over several years in the industry, coupled with dedicated coursework and diverse projects, I've honed my skills across a wide range
+                Over several years in the industry, coupled with dedicated coursework and diverse projects, I've honed my skills across a wide range
                 of technologies, including Javascript, Python, CSS, HTML, React, Node.js, Express.js, SQL, JQuery, C++, Assembly, and Java. With 
                 expertise in both front-end and back-end technologies, I thrive in dynamic environments where innovation and collaboration are 
                 paramount.
                 <br/> <br/>
-                &emsp; Beyond the world of software, I enjoy an active lifestyle, whether catching waves along the local coastline, competing on the 
-                pickleball court, or kicking a soccer ball around. Recently, I've embarked on a journey to learn Spanish, further enriching my 
+                Beyond the world of software, I enjoy an active lifestyle - whether surfing waves along the local coastline, competing on the 
+                pickleball courts, or playing soccer. Recently, I've embarked on a journey to learn Spanish, further enriching my 
                 personal and professional horizons.
                 <br/> <br/>
             </p>
