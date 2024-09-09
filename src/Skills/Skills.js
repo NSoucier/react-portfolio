0f31dfb602765739e1508c4bcf1daba9a79638a6
Nathan Soucier <soucienp@gmail.com>
2024-09-09 01:10:56 -0400

    updated backgrounds

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -3,7 +3,7 @@ import './Skills.css';
 function Skills() {
     return (
         <div id='skills'>
-            <h1>Tech Stack</h1>
+            <h1>Skills</h1>
             <p>JavaScript, SQL, ReactJS, Nodejs, Expressjs, TypeScript, Redux, jQuery, CSS/HTML, C/C++, Python, Java, GitHub Assembly, Jira, MATLAB, Git, Agile, SolidWorks, CRM
             </p>
         </div>
