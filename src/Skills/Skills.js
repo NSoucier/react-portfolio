609d9d691f0fa98bbff6126ca7198b24dbc53d3c
Nathan Soucier <soucienp@gmail.com>
2024-09-09 05:58:51 -0400

    updated skills and responsiveness

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,10 +1,97 @@
 import './Skills.css';
+import { Card, CardBody, CardText, Container, Row, Col } from 'reactstrap';
+import js from '../Images/js.png';
+import py from '../Images/py.png';
+import node from '../Images/node.png';
+import react from '../Images/react.png';
+import ex from '../Images/ex.png';
+import psql from '../Images/psql.png';
+import html from '../Images/html.png';
+import css from '../Images/css.png';
 
 function Skills() {
     return (
         <div id='skills'>
             <h1>Skills</h1>
-            <p>JavaScript, SQL, ReactJS, Nodejs, Expressjs, TypeScript, Redux, jQuery, CSS/HTML, C/C++, Python, Java, GitHub Assembly, Jira, MATLAB, Git, Agile, SolidWorks, CRM
+                <Row>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={js} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={py} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={node} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={react} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={ex} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={psql} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={html} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className='skillCard'>
+                            <CardBody>
+                                <CardText>
+                                    <img src={css} className='logos' />
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </Col>                                                                                                                        
+                </Row>
+            <p>
+                <b>Other skills:</b> TypeScript, Angular, Redux, C/C++, Java, Git, Assembly, Jira, Agile, SolidWorks, MATLAB, CRM
+            </p>
+            <p>
+                <b>Spoken languages:</b> English, French (Bilingual), Spanish (Beginner)
             </p>
         </div>
     )
