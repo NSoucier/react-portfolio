818fd41be8005391d3f312ea7bc02f85a1c71ac5
Nathan Soucier <soucienp@gmail.com>
2024-11-05 16:19:22 -0500

    added copyright name in footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,11 +5,17 @@ import email from '../Images/email.png';
 
 function Footer() {
     return (
-        <div id="footer">
-            <a href='https://github.com/NSoucier' target="_blank" rel="noopener noreferrer"><img src={github} className='socials' alt='github link'/> </a>
-            <a href='https://www.linkedin.com/in/soucier/' target="_blank" rel="noopener noreferrer"><img src={linkedin} className='socials' alt='linkedin link'/> </a>
-            <a href='mailto:soucienp@gmail.com'><img src={email} className='socials' alt='email link'/> </a>
-        </div>
+        <>
+            <div id="footer">
+                <a href='https://github.com/NSoucier' target="_blank" rel="noopener noreferrer"><img src={github} className='socials' alt='github link'/> </a>
+                <a href='https://www.linkedin.com/in/soucier/' target="_blank" rel="noopener noreferrer"><img src={linkedin} className='socials' alt='linkedin link'/> </a>
+                <a href='mailto:soucienp@gmail.com'><img src={email} className='socials' alt='email link'/> </a>
+            </div>
+
+            <div id="copyright">
+                © Nathan Soucier
+            </div>             
+        </>
     )
 }
 
